c028adeb3f5c137e22847668666d6ca6565b3349
Izaac Molina <izaac.6.30@gmail.com>
2023-07-19 21:12:26 +0000

    Added pseudocode for main, implemented form for search settings

--- app.py
+++ app.py
@@ -10,19 +10,34 @@ app.config['SECRET_KEY'] = '16bd5547b4e8139970845e9f58c7e470'
 @app.route("/")
 @app.route("/landing")
 def landing():
+    # if signup button is pressed
+
+        # redirect to signup page
+
+    # if login button is pressed
+
+        # redirect to login page
+
     return "<p>Welcome to the landing page</p>" 
 
 
 # define user login page
 @app.route("/login")
 def login():
+
+    # grab form data for login
     form = LoginForm()
 
+    # if login button is pressed
+
+        # redirect to login page
+
+    # validate login form on submit
     if form.validate_on_submit():
             
-        # Obtain username and password from form
+        # obtain username and password from form
         
-        # Check that username is exists, password matches
+        # check that username is exists, password matches
         
         # If invalid, redirect to login, link to sign up
 
@@ -32,17 +47,23 @@ def login():
 
 
 # define signup page
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    # grab form data for signup
     form = RegistrationForm()
 
+    # if sign up button is pressed
+
+        # redirect to sign up page
+
+    # validate signup form on submit
     if form.validate_on_submit():
         
-        # Obtain username and email data from form
+        # obtain username and email data from form
         
-        # Check that username and email are unique
+        # check that username and email are unique
         
-        # If not unique, redirect to signup page
+        # if not unique, redirect to signup page
 
         # add user data to database
 
@@ -51,24 +72,32 @@ def signup():
 
 
 # define home page
-@app.route("/home")
+@app.route("/home", methods=['GET', 'POST'])
 def home():
+    # grab form data for news search
+    form = SearchForm()
+
+    # validate search form on submit
+    if form.validate_on_submit():
+
+        # make call to api to retrieve news stories based on form
+
     return "<p>Welcome to the home page</p>"
 
 
 # define tracking page
-@app.route("/tracking")
+@app.route("/tracking", methods=['GET', 'POST'])
 def tracking():
     return "<p>Welcome to the tracking page</p>"
 
 
 # define topic expanding page
-@app.route("/topic_expand")
+@app.route("/topic_expand", methods=['GET', 'POST'])
 def topic_expand():
     return "<p>Welcome to the topic expansion page</p>"
 
 # define bookmarks page
-@app.route("/bookmarks")
+@app.route("/bookmarks", methods=['GET', 'POST'])
 def bookmarks():
     return "<p>Welcome to the bookmarks page</p>" 
 
---
+++ forms.py
@@ -0,0 +1,121 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms import BooleanField, IntegerField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from from datetime import date
+
+# list of valid countries to be searched from using search api
+# the two letter codes are needed to call the api
+valid_countries = {
+    'United Arab Emirates': 'ae',
+    'Argentina': 'ar',
+    'Austria': 'at',
+    'Australia': 'au',
+    'Belgium': 'be',
+    'Bulgaria': 'bg',
+    'Brazil': 'br',
+    'Canada': 'ca',
+    'Switzerland': 'ch',
+    'China': 'cn',
+    'Colombia': 'co',
+    'Cuba': 'cu',
+    'Czech Republic': 'cz',
+    'Germany': 'de',
+    'Egypt': 'eg',
+    'France': 'fr',
+    'United Kingdom': 'gb',
+    'Greece': 'gr',
+    'Hong Kong': 'hk',
+    'Hungary': 'hu',
+    'Indonesia': 'id',
+    'Ireland': 'ie',
+    'Israel': 'il',
+    'India': 'in',
+    'Italy': 'it',
+    'Japan': 'jp',
+    'South Korea': 'kr',
+    'Lithuania': 'lt',
+    'Latvia': 'lv',
+    'Morocco': 'ma',
+    'Mexico': 'mx',
+    'Malaysia': 'my',
+    'Nigeria': 'ng',
+    'Netherlands': 'nl',
+    'Norway': 'no',
+    'New Zealand': 'nz',
+    'Philippines': 'ph',
+    'Poland': 'pl',
+    'Portugal': 'pt',
+    'Romania': 'ro',
+    'Russia': 'ru',
+    'Saudi Arabia': 'sa',
+    'Sweden': 'se',
+    'Singapore': 'sg',
+    'Slovenia': 'si',
+    'Slovakia': 'sk',
+    'Thailand': 'th',
+    'Turkey': 'tr',
+    'Taiwan': 'tw',
+    'Ukraine': 'ua',
+    'United States': 'us',
+    'Venezuela': 've'
+}
+
+
+valid_languages = {
+    'Arabic': 'ar',
+    'German': 'de',
+    'English': 'en',
+    'Spanish': 'es',
+    'French': 'fr',
+    'Hebrew': 'he',
+    'Italian': 'it',
+    'Dutch': 'nl',
+    'Norwegian': 'no',
+    'Portuguese': 'pt',
+    'Russian': 'ru',
+    'Swedish': 'sv',
+    'Ukrainian': 'uk',
+    'Danish': 'da',
+    'Chinese': 'zh'
+}
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=20)])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), Length(min=5, max=20), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=5, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class SearchForm(FlaskForm):
+
+    # checks if the inputed country is one of the supported countries
+    def validate_nation(form, field):
+        if field.data not in valid_countries:
+            raise ValidationError("Invalid country, or country is not supported")
+    
+    # checks if the inputed language is one of the supported languages
+    def validate_nation(form, field):
+        if field.data not in valid_languages:
+            raise ValidationError("Invalid language, or language is not supported")
+
+    # define form inputs
+    search_input = StringField(validators=[DataRequired(), Length(min=1)])
+    date = DateField('Starting From', format = '%Y-%m-%d', default = date.today())
+    nation = StringField('Country', default = 'United States', validators=[validate_nation, Length(min=1, max=50)])
+    language = StringField('Language', default = 'United States', validators=[validate_language, Length(min=1, max=50)])
+    update_interval = IntegerField('Update every __ day(s)', NumberRange(min=1, max=31))
+    source = StringField('News Source', validators=[Length(min=1, max=50)])
+    submit = SubmitField('Search')
